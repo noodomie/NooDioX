352949af354bbfa0f1ddc5a84a3a42439ea0a224
noodomie <noodomie@gmail.com>
2026-07-29 15:57:50 +0300

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,4 +1,3 @@
-cat << 'EOF' > public/game.js
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 const minimapCanvas = document.getElementById('minimapCanvas');
@@ -381,4 +380,3 @@ function escapeHTML(str) {
 }
 
 requestAnimationFrame(render);
-EOF
